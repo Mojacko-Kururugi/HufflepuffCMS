d7014a16d5fc3eb1cdd2276fd7ecc07a25bd0fd0
kcNeko <kriziacamilleuayan@gmail.com>
2017-02-10 18:54:26 +0800

    try

--- app/views/add-doctor.blade.php
+++ app/views/add-doctor.blade.php
@@ -4,7 +4,7 @@
 
   <div class="row page-title">
     <div class="col s12 m12 l12">
-      <h4>Doctor Records</h4>
+      <h4>Add New Doctor</h4>
     </div>
   </div>
 
@@ -12,88 +12,88 @@
   <div class="card">
     <div class="card-content">
         <div class="contents">
-    <div class="container-fluid">
-      <form action="{{ URL::to('teacher/save-profile-edit') }}" method="POST" id="signup_validate" enctype="multipart/form-data"><br><br>
-        <div class="row">
-          <div class="input-field col l6 m6 s12">
-            <input id="user_id" name="user_id" type="text" class="validate" data-error=".id_error" value="" readonly/>
-            <label for="user_id">License ID:</label>
-            <div class="id_error"></div>
-          </div>
+          <div class="container-fluid">
+            <form action="{{ URL::to('teacher/save-profile-edit') }}" method="POST" id="signup_validate" enctype="multipart/form-data">
+              <div class="row">
+                <div class="input-field col l6 m6 s12">
+                  <input id="user_id" name="user_id" type="text" class="validate" data-error=".id_error" value="" readonly/>
+                  <label for="user_id">License ID:</label>
+                  <div class="id_error"></div>
+                </div>
+              </div>
+              <div class="row">
+                    <div class="input-field col s12 m6 l6">
+                      <input id="role" type="text" class="validate" value="Doctor" readonly>
+                      <label for="role">User Type</label>
+                    </div>
+              </div>
+              <div class="row">
+                <div class="col s12"><br>
+                  <div class="row" style="padding:0px; margin:0px;">
+                    <p style="padding:0px; margin:0px;">Name:</p>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12 m4 l4">
+                      <input id="last_name_sa" name="last_name_sa" type="text" class="validate" value="" pattern="[A-Za-z]+" onkeydown="return alphaOnly(event);">
+                      <label for="last_name_sa">Last Name</label>
+                    </div>
+                    <div class="input-field col s12 m4 l4">
+                      <input id="first_name_sa" name="first_name_sa" type="text" class="validate" value="" pattern="[A-Za-z]+" onkeydown="return alphaOnly(event);">
+                      <label for="first_name_sa">First Name</label>
+                    </div>
+                    <div class="input-field col s12 m4 l4">
+                      <input id="middle_name_sa" name="middle_name_sa" type="text" class="validate" value="" pattern="[A-Za-z]+" onkeydown="return alphaOnly(event);">
+                      <label for="middle_name_sa">Middle Name</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col s12">
+                      <label for="gender_select">Gender</label>
+                      <p>
+                        <input name="gender" type="radio" id="male" value="Male"/>
+                        <label for="male">Male</label>
+                      </p>                         
+                      <p>
+                        <input name="gender" type="radio" id="female" value="Female" checked/>
+                        <label for="female">Female</label>
+                      </p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12 m12 l12">
+                      <input id="address" name="address" type="text" class="validate" value="">
+                      <label for="address">Address</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col s12 m8 l6">
+                      <label for="b_day" >Date Hired</label>
+                      <input id="b_day" name="b_day" type="date" class="datepicker" style="height:39px" value="">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12 m8 l6">
+                      <input id="stud_id_no" name="stud_id_no" type="text" class="validate" value="">
+                      <label for="stud_id_no">Contact Number</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col l6 m8 s12">
+                      <label for="email">Branch</label>
+                      <input id="number" name="number" type="text" class="validate" value="" />
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12 center">
+                      <button type="submit" class="waves-effect waves-light btn blue darken-1 modal-btn">Save</button>
+                      <a href="{{ URL::to('/doctors') }}" class="waves-effect waves-light btn btn-green modal-btn" style="margin-right:20px;">Cancel</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div> 
         </div>
-        <div class="row">
-              <div class="input-field col s12 m6 l6">
-                <input id="role" type="text" class="validate" value="Doctor" readonly>
-                <label for="role">User Type</label>
-              </div>
-        </div>
-        <div class="row">
-          <div class="col s12"><br>
-            <div class="row" style="padding:0px; margin:0px;">
-              <p style="padding:0px; margin:0px;">Name:</p>
-            </div>
-            <div class="row">
-              <div class="input-field col s12 m4 l4">
-                <input id="last_name_sa" name="last_name_sa" type="text" class="validate" value="" pattern="[A-Za-z]+" onkeydown="return alphaOnly(event);">
-                <label for="last_name_sa">Last Name</label>
-              </div>
-              <div class="input-field col s12 m4 l4">
-                <input id="first_name_sa" name="first_name_sa" type="text" class="validate" value="" pattern="[A-Za-z]+" onkeydown="return alphaOnly(event);">
-                <label for="first_name_sa">First Name</label>
-              </div>
-              <div class="input-field col s12 m4 l4">
-                <input id="middle_name_sa" name="middle_name_sa" type="text" class="validate" value="" pattern="[A-Za-z]+" onkeydown="return alphaOnly(event);">
-                <label for="middle_name_sa">Middle Name</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col s12">
-                <label for="gender_select">Gender</label>
-                <p>
-                  <input name="gender" type="radio" id="male" value="Male"/>
-                  <label for="male">Male</label>
-                </p>                         
-                <p>
-                  <input name="gender" type="radio" id="female" value="Female" checked/>
-                  <label for="female">Female</label>
-                </p>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12 m12 l12">
-                <input id="address" name="address" type="text" class="validate" value="">
-                <label for="address">Address</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col s12 m8 l6">
-                <label for="b_day" >Date Hired</label>
-                <input id="b_day" name="b_day" type="date" class="datepicker" style="height:39px" value="">
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12 m8 l6">
-                <input id="stud_id_no" name="stud_id_no" type="text" class="validate" value="">
-                <label for="stud_id_no">Contact Number</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col l6 m8 s12">
-                <label for="email">Branch</label>
-                <input id="number" name="number" type="text" class="validate" value="" />
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12 center">
-                <button type="submit" class="waves-effect waves-light btn btn-green modal-btn">Save</button>
-                <a href="{{ URL::to('/doctors') }}" class="waves-effect waves-light btn btn-green modal-btn" style="margin-right:20px;">Cancel</a>
-              </div>
-            </div>
-          </div>
-        </div>
-    </form>
-  </div> 
-  </div><br><br>
     </div>
   </div>
 </div>
